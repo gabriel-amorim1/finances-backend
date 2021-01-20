d1b73b7cf6827dac3686475b6fe9e3f86704292a
Gabriel Amorim <therealamorim@gmail.com>
2021-01-20 16:19:37 -0300

    Feat: Financial Movement CRUD Service

--- src/services/FinancialMovementService.ts
+++ src/services/FinancialMovementService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { DeleteResult } from 'typeorm';
 import FinancialMovement from '../database/entities/FinancialMovement';
 import { FinancialMovementInterface } from '../interfaces/FinancialMovementInterface';
 import IFinancialMovementRepository from '../interfaces/repositories/IFinancialMovementRepository';
@@ -34,6 +35,29 @@ class FinancialMovementService {
             throw new HttpError(404, 'Financial Movement not found');
 
         return financialMovement;
+    }
+
+    public async getAll(): Promise<{ data: FinancialMovement[]; count: number }> {
+        const users = await this.financialMovementRepository.getAll();
+
+        return users;
+    }
+
+    public async update(
+        id: string,
+        movementUpdate: FinancialMovementInterface,
+    ): Promise<FinancialMovement> {
+        const foundMovement = await this.findById(id);
+
+        return this.financialMovementRepository.update(
+            Object.assign(foundMovement, { ...movementUpdate }),
+        );
+    }
+
+    public async remove(id: string): Promise<DeleteResult> {
+        await this.findById(id);
+
+        return this.financialMovementRepository.remove(id);
     }
 }
 
--- src/tests/services/financialMovement.spec.ts
+++ src/tests/services/financialMovement.spec.ts
@@ -7,6 +7,7 @@ import FakeFinancialMovementRepository from '../repositories/fakes/FakeFinancial
 import UserService from '../../services/UserService';
 import FakeUserRepository from '../repositories/fakes/FakeUserRepository';
 import { UserInterface } from '../../interfaces/UserInterface';
+import { HttpError } from '../../utils/errors/HttpError';
 
 describe('FinancialMovement Service', () => {
     let financialMovementService: FinancialMovementService;
@@ -88,5 +89,75 @@ describe('FinancialMovement Service', () => {
         const res = await financialMovementService.findById(sut.id);
 
         expect(res).toEqual(sut);
+    });
+
+    it('should be able to get all FinancialMovements', async () => {
+        const sut1 = await makeSut();
+        const sut2 = await makeSut();
+
+        const expectedRes = {
+            data: [sut1, sut2],
+            count: 2,
+        };
+
+        const res = await financialMovementService.getAll();
+
+        expect(res).toEqual(expectedRes);
+    });
+
+    it('should be able to get all FinancialMovement and return a empty array', async () => {
+        const expectedRes = {
+            data: [],
+            count: 0,
+        };
+
+        const res = await financialMovementService.getAll();
+
+        expect(res).toEqual(expectedRes);
+    });
+
+    it('should be able to update a FinancialMovement by id', async () => {
+        const sut = await makeSut();
+
+        const value = 321.05;
+
+        const expectedRes = <FinancialMovement>{
+            ...sut,
+            value,
+        };
+
+        const res = await financialMovementService.update(sut.id, <any>{ value });
+
+        expect(res).toEqual(expectedRes);
+    });
+
+    it('should not to be able to update a FinancialMovement by id', async () => {
+        expect.hasAssertions();
+
+        try {
+            await financialMovementService.update(v4(), <any>{});
+        } catch (error) {
+            expect(error).toBeInstanceOf(HttpError);
+            expect(error.message).toBe('Financial Movement not found');
+        }
+    });
+
+    it('should be able to remove a FinancialMovement by id', async () => {
+        const sut = await makeSut();
+
+        const res = await financialMovementService.remove(sut.id);
+
+        expect(res).toEqual({ raw: [] });
+    });
+
+    it('should not to be able to remove a FinancialMovement by id', async () => {
+        expect.hasAssertions();
+
+        try {
+            await financialMovementService.remove(v4());
+        } catch (error) {
+            expect(error).toBeInstanceOf(HttpError);
+            expect(error.message).toBe('Financial Movement not found');
+        }
     });
 });
