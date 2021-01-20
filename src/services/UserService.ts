d2387ffe953a77095097b6c9f6966703455e363e
Gabriel Amorim <therealamorim@gmail.com>
2021-01-20 15:46:35 -0300

    Feat: User CRUD Service

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { DeleteResult } from 'typeorm';
 import User from '../database/entities/User';
 import IUserRepository from '../interfaces/repositories/IUserRepository';
 import { UserInterface } from '../interfaces/UserInterface';
@@ -21,6 +22,26 @@ class UserService {
         if (!user) throw new HttpError(404, 'User not found');
 
         return user;
+    }
+
+    public async getAll(): Promise<{ data: User[]; count: number }> {
+        const users = await this.userRepository.getAll();
+
+        return users;
+    }
+
+    public async update(id: string, userUpdate: UserInterface): Promise<User> {
+        const foundUser = await this.findById(id);
+
+        return this.userRepository.update(
+            Object.assign(foundUser, { ...userUpdate }),
+        );
+    }
+
+    public async remove(id: string): Promise<DeleteResult> {
+        await this.findById(id);
+
+        return this.userRepository.remove(id);
     }
 }
 
--- src/tests/services/user.spec.ts
+++ src/tests/services/user.spec.ts
@@ -4,6 +4,7 @@ import UserService from '../../services/UserService';
 import User from '../../database/entities/User';
 import { UserInterface } from '../../interfaces/UserInterface';
 import FakeUserRepository from '../repositories/fakes/FakeUserRepository';
+import { HttpError } from '../../utils/errors/HttpError';
 
 describe('User Service', () => {
     let userService: UserService;
@@ -58,5 +59,89 @@ describe('User Service', () => {
         const res = await userService.findById(sut.id);
 
         expect(res).toEqual(expectedRes);
+    });
+
+    it('should not to be able to find a User by id', async () => {
+        expect.hasAssertions();
+
+        try {
+            await userService.findById(v4());
+        } catch (error) {
+            expect(error).toBeInstanceOf(HttpError);
+            expect(error.message).toBe('User not found');
+        }
+    });
+
+    it('should be able to get all Users', async () => {
+        const sut1 = await makeSut();
+        const sut2 = await makeSut();
+
+        const expectedRes = {
+            data: [sut1, sut2],
+            count: 2,
+        };
+
+        const res = await userService.getAll();
+
+        expect(res).toEqual(expectedRes);
+    });
+
+    it('should be able to get all Users and return a empty array', async () => {
+        const expectedRes = {
+            data: [],
+            count: 0,
+        };
+
+        const res = await userService.getAll();
+
+        expect(res).toEqual(expectedRes);
+    });
+
+    it('should be able to update a User by id', async () => {
+        const sut = await makeSut();
+
+        const email = 'gabriel@update.com';
+
+        const expectedRes = <User>{
+            id: sut.id,
+            name: 'Gabriel',
+            email: 'gabriel@update.com',
+            created_at: sut.created_at,
+            updated_at: sut.updated_at,
+        };
+
+        const res = await userService.update(sut.id, <any>{ email });
+
+        expect(res).toEqual(expectedRes);
+    });
+
+    it('should not to be able to update a User by id', async () => {
+        expect.hasAssertions();
+
+        try {
+            await userService.update(v4(), <any>{});
+        } catch (error) {
+            expect(error).toBeInstanceOf(HttpError);
+            expect(error.message).toBe('User not found');
+        }
+    });
+
+    it('should be able to remove a User by id', async () => {
+        const sut = await makeSut();
+
+        const res = await userService.remove(sut.id);
+
+        expect(res).toEqual({ raw: [] });
+    });
+
+    it('should not to be able to remove a User by id', async () => {
+        expect.hasAssertions();
+
+        try {
+            await userService.remove(v4());
+        } catch (error) {
+            expect(error).toBeInstanceOf(HttpError);
+            expect(error.message).toBe('User not found');
+        }
     });
 });
